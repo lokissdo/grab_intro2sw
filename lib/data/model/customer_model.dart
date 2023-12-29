24b6e9d520f85ee8ce142bceb61876442cb60da8
hung <dokhaihung2003@gmai.com>
2023-12-29 16:49:03 +0700

    add promotioms to CustomerModel

--- lib/data/model/customer_model.dart
+++ lib/data/model/customer_model.dart
@@ -1,19 +1,22 @@
 
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:grab/data/model/address_model.dart';
+import 'package:grab/data/model/promotion_model.dart';
 
 class CustomerModel {
   static String collectionName = 'customers';
-  CustomerModel(
-      {required this.name,
-      required this.id,
-      required this.phoneNumber,
-      required this.email,
-       this.address,
-      this.createdAt,
-      
-      this.updatedAt,
-      this.isDeleted = false});
+ CustomerModel({
+    required this.name,
+    required this.id,
+    required this.phoneNumber,
+    required this.email,
+    this.address,
+    this.createdAt,
+    this.updatedAt,
+    this.isDeleted = false,
+    List<PromotionModel>? promotions, // Add list of promotions
+  }): promotions = promotions ?? [] ; // Initialize promotion list
+
   String id;
   String name;
   String phoneNumber;
@@ -21,6 +24,7 @@ class CustomerModel {
   Timestamp? updatedAt;
   String email;
   AddressModel? address;
+  List<PromotionModel> promotions;
   bool isDeleted ;
 
   static CustomerModel fromJson(Map<String, dynamic> map) {
@@ -33,6 +37,10 @@ class CustomerModel {
       updatedAt: map["updatedAt"] as Timestamp?,
       isDeleted: map["isDeleted"] as bool,
       address: map["address"] != null ? AddressModel.fromJson(map["address"]) : null, // Use Address.fromJson()
+      promotions: (map['promotions'] as List<dynamic>?)
+                  ?.map((e) => PromotionModel.fromJson(e))
+                  .toList() ??
+              [],
     );
   }
 
@@ -46,6 +54,7 @@ class CustomerModel {
       "createdAt": createdAt,
       "updatedAt": updatedAt,
       "isDeleted": isDeleted,
+       "promotions": promotions.map((e) => e.toJson()).toList(),
     };
   }
 }
